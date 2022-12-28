83a9320fdc7bfd22d9354bb897fd5adbaf2793ad
Emilie <emilie.rich4rd@gmail.com>
2022-12-28 12:14:45 +0100

    images skills changes

--- src/data/skills.js
+++ src/data/skills.js
@@ -33,7 +33,7 @@ export const portfolio = [
     description:
       "Projet SEE/SEO",
     details: "La Panthère est un site vitrine d'une agence de web design. Le but de ce projet était de corriger une version donnée du site pour améliorer ses performances et corriger les mauvaises pratiques appliquées. J'ai utilisé l'outil Lighthouse de Google Chrome pour analyser le changement des performances en fonction de mes corrections",
-    pictures:["../media/skills/Lapanthere.PNG", "../media/skills/Lapanthere2.PNG", "../media/skills/Lapanthere3.PNG"],
+    pictures:["../media/skills/Lapanthere.PNG", "../media/skills/Lapanthere2.PNG", "../media/skills/Lapanthere3.PNG", "../media/skills/Lapanthere4.PNG"],
     link: "https://emilie2393.github.io/Projet_3_Panthere/"
   },
   {
@@ -44,7 +44,7 @@ export const portfolio = [
     description:
       "Intégration dynamique d'une API",
     details: "Kanap est un site e-commerce dynamique de vente de canapés. Il fallait ici intégrer les élements de l'API sur les pages HTML à l'aide de Javascript. Pour cela, j'ai créé 4 fichiers Javascript qui fournissait le DOM des informations nécessaires. L'API renvoyait les informations de chaque canapé. Le challenge ici était d'implémenter des fonctions asynchrones qui fonctionnaient indépendamment les unes des autres. Le local storage est utilisé pour sauvegarder les informations du panier de l'utilisateur.Enfin, des RegEx sont utilisées pour sécuriser le formulaire du panier. ",
-    pictures:["../media/skills/Kanap.PNG", "../media/skills/Kanap2.PNG", "../media/skills/Kanap3.PNG"],
+    pictures:["../media/skills/Kanap.PNG", "../media/skills/Kanap2.PNG", "../media/skills/Kanap3.PNG", "../media/skills/Kanap4.PNG", "../media/skills/Kanap5.PNG"],
     link: "https://github.com/Emilie2393/Projet_4_Kanap"
   },
   {
@@ -55,7 +55,7 @@ export const portfolio = [
     description:
       "Création d'une API sécurisée",
     details: "Le projet de Hot Takes est la création complète d'une API. Cette plateforme sers à publier sa sauce préférée et d'aimer ou pas les publications. Il faut pour cela créer un compte utilisateur à l'aide d'une adresse mail et un mot de passe. Le mot de passe est haché et la connextion génère un token vérifié à chaque chargement de page. Cette application fonctionne à la date de la base de donnée MongoDB reliée au backend. Les utilisateurs et les produits créés sont enregistrés sur cette base de données. ",
-    pictures:["../media/skills/Hottakes.PNG","../media/skills/Hottakes2.PNG", "../media/skills/Hottakes3.PNG", "../media/skills/Hottakes4.PNG"],
+    pictures:["../media/skills/Hottakes.PNG","../media/skills/Hottakes2.PNG", "../media/skills/Hottakes3.PNG", "../media/skills/Hottakes4.PNG", "../media/skills/Hottakes5.PNG"],
     link: "https://github.com/Emilie2393/Projet_5_HotTakes"
   },
   {
@@ -66,7 +66,7 @@ export const portfolio = [
     description:
       "Implémentation front-end avec React",
     details: "Kasa est un site de location d'appartement entre particuliers. Le projet ici était de transformer l'application avec React. Le site est transcris à travers des composants sur le fichier App. React Router organise les routes de l'application. Une page d'erreur est présentée en cas de mauvaise URL.",
-    pictures:["../media/skills/Kasa.PNG","../media/skills/Kasa2.PNG", "../media/skills/Kasa3.PNG"],
+    pictures:["../media/skills/Kasa.PNG","../media/skills/Kasa2.PNG", "../media/skills/Kasa3.PNG", "../media/skills/Kasa4.PNG"],
     link: "https://github.com/Emilie2393/Projet_6_Kasa"
   }
 
