348215e03cb6a6a1d3c68f0631519f0bf9bb3b72
Emilie <emilie.rich4rd@gmail.com>
2023-01-14 14:50:59 +0100

    Nav browser correction

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/components/Navigation.scss"
 
 
 const Navigation = () => {
+
+    const [showNav, setShowNav] = useState(false)
+
+    const handleShowNav = () => {
+        setShowNav(!showNav)
+    }
+
     return (
-        <div className="nav">
+        <div className={` ${showNav ? "nav" : "nav__hidden"} `}>
+            <button className="nav__burger" onClick={handleShowNav}>
+                <div className="nav__burger__bar"></div>
+            </button>
             <Link to="/" className="nav__home"><p>Accueil</p></Link>
             <Link to="/skills" className="nav__skills"><p>Portfolio</p></Link>
             <Link to="/hobbies" className="nav__hobbies"><p>Hobbies</p></Link>
