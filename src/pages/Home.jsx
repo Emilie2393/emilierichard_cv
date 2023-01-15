4c09c2b87e6f825d9b004f6ae41a1cec5df120a6
Emilie <emilie.rich4rd@gmail.com>
2023-01-15 17:51:04 +0100

    home text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
             <h1 className="home__firstpart__title">Bienvenue</h1>
             <h2>
               Je m'appelle Emilie Richard, et je recherche un poste de
-              Développeur Front-End
+              Développeur Junior
             </h2>
             <a href="../../CV.pdf" download>Télécharger mon CV</a>
           </div>
@@ -58,13 +58,13 @@ const Home = () => {
           </div>
           <div className="home__fourthpart__text">
             <p>
-              J'ai pu étudier le language Python dans un premier temps sur la
+              J'ai pu étudier le langage Python dans un premier temps sur la
               plateforme MOOC. Puis j'ai pris contact avec OPENCLASSROOMS pour
               pousser le projet plus loin. En 2021 je prend la décision de
-              quitter mon poste en CDI pour changer de vie professionnelle.
-              Aujourd'hui j'ai validé un Titre RNCP de niveau 5 Développeur Web
+              quitter mon CDI pour changer de vie professionnelle.
+              Aujourd'hui j'ai validé un titre RNCP de niveau 5 Développeur Web
               et je me présente devant vous pour poursuivre cette voie et
-              apporter le maximum à votre entreprise. Bienvenue sur mon site !
+              apporter le maximum à votre entreprise. 
             </p>
           </div>
         </div>
